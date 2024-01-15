942d5a401a3dbee5141f637599719e67fcf41583
CalebHite <96922867+CalebHite@users.noreply.github.com>
2024-01-15 15:32:30 -0600

    Implement Firebase

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    export const ssr = false;
+    import { app } from './firebase';
+    import { collectionStore } from 'sveltefire';
+    import { getFirestore  } from "firebase/firestore";
+    // import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  
+    const firestore = getFirestore(app);
+
+    const pictures = collectionStore(firestore, 'pictures');
+  
+    // Add this when creating the page to add pictures.
+    //  \/\/\/\/\/
+    // function newItem(id: number, name: string, desc: string, time: number, completeBy: string) {
+    //   if(name != "" && desc != "" && time !> 0 && time != null && completeBy != ""){
+    //     addDoc(collection(firestore, "items"), {
+    //     name: name,
+    //     desc: desc,
+    //     time: time,
+    //     completeBy: completeBy
+    //   });
+    //   }
+    // }
+  
+    // function delItem(id: string){
+    //   deleteDoc(doc(firestore, "items", id));
+    // }
+  
+  </script>
+    <p>ATO Gamma Mu</p> 
+    {#each $pictures as p}
+        <img src="{p.url}" alt="{p.alt}">
+    {/each}
--- src/routes/firebase.ts
+++ src/routes/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// If this shit isnt working type 'npm i' into your terminal
+// If this shit isnt working type 'npm i' into your terminal inside of the 'routes' folder
 
 const firebaseConfig = {
   apiKey: "AIzaSyBh1UFfDoC9meS3PaU-ZC7Rujm7cidbyXI",
